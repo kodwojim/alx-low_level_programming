78a276c02632dc43d3e0f9529879a417fabdf391
Jimmy Kodwo Essel <kodwoessel41@gmail.com>
2023-03-03 10:11:10 -0800

    alx low level programming

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+/**
+ * reverse_array - reverse array
+ *
+ * @a: content to be reversed
+ *
+ * @n: n is the number of elements of the array
+ *
+ * Return: none
+ */
+
+void reverse_array(int *a, int n)
+{
+	int x, y;
+
+	/*where the magic happens*/
+	for (x = 0; (x < (n - 1) / 2); x++)
+	{
+		y = a[x];
+		a[x] = a[n - 1 - x];
+		a[n - 1 - x] = y;
+	}
+}
